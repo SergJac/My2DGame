ea6e2fe0610e70d1b035d82cf2e66f631fc3f834
SergJac <sergiogjacquet@gmail.com>
2025-05-08 14:03:08 -0400

    NOC

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -11,16 +11,15 @@ import java.io.IOException;
 
 public class Player extends Entity{
 
-    GamePanel gp;
     KeyHandler keyH;
     public final int screenX;
     public final int screenY;
-//    public int hasKey = 0;
     int standCounter = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
 
-        this.gp = gp;
+        super(gp);
+
         this.keyH = keyH;
 
         screenX = gp.screenWidth/2 - (gp.tileSize/2);
@@ -47,29 +46,14 @@ public class Player extends Entity{
 
     public void getPlayerImage(){
 
-        up1 = setup("boy_up_1");
-        up2 = setup("boy_up_2");
-        down1 = setup("boy_down_1");
-        down2 = setup("boy_down_2");
-        left1 = setup("boy_left_1");
-        left2 = setup("boy_left_2");
-        right1 = setup("boy_right_1");
-        right2 = setup("boy_right_2");
-    }
-
-    public BufferedImage setup(String imageName){
-
-        UtilityTool uTool = new UtilityTool();
-        BufferedImage image = null;
-
-        try {
-            image = ImageIO.read(getClass().getResourceAsStream("/player/" + imageName + ".png"));
-            image = uTool.scaledImage(image, gp.tileSize, gp.tileSize);
-
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-        return image;
+        up1 = setup("/player/boy_up_1");
+        up2 = setup("/player/boy_up_2");
+        down1 = setup("/player/boy_down_1");
+        down2 = setup("/player/boy_down_2");
+        left1 = setup("/player/boy_left_1");
+        left2 = setup("/player/boy_left_2");
+        right1 = setup("/player/boy_right_1");
+        right2 = setup("/player/boy_right_2");
     }
 
     public void update() {
@@ -97,6 +81,10 @@ public class Player extends Entity{
             // CHECK OBJECT COLLISION
             int objIndex = gp.cChecker.checkObject(this, true);
             pickUpObject(objIndex);
+
+            // CHECK NPC COLLISION
+            int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
+            interactNPC(npcIndex);
 
             // IF COLLISION IS FALSE, PLAYER CAN MOVE
             if(collisionOn == false){
@@ -137,11 +125,14 @@ public class Player extends Entity{
 
         }
     }
+    public void interactNPC(int i){
+
+        if (i != 999){
+            System.out.println("You are hitting an NPC!");
+        }
+    }
 
     public void draw(Graphics2D g2){
-
-//        g2.setColor(Color.white);
-//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
 
         BufferedImage image = null;
 
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import entity.Entity;
 import entity.Player;
 import object.SuperObject;
 import tile.TileManager;
@@ -39,6 +40,7 @@ public class GamePanel extends JPanel implements Runnable{
     // ENTITY AND OBJECT
     public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
+    public Entity npc[] = new Entity[10];
 
     // GAME STATE
     public int gameState;
@@ -57,6 +59,7 @@ public class GamePanel extends JPanel implements Runnable{
     public void setupGame(){
 
         aSetter.setObject();
+        aSetter.setNPC();
         playMusic(0);
         stopMusic();
         gameState = playState;
@@ -105,7 +108,15 @@ public class GamePanel extends JPanel implements Runnable{
     public void update() {
 
         if(gameState == playState) {
+            // PLAYER
             player.update();
+            // NPC
+            for(int i = 0; i < npc.length; i++) {
+                if(npc[i] != null) {
+                    npc[i].update();
+                }
+            }
+
         }
         if(gameState == pauseState) {
             // nothing
@@ -131,6 +142,14 @@ public class GamePanel extends JPanel implements Runnable{
             if(obj[i] != null){
                 obj[i].draw(g2, this);
             }
+        }
+
+        // NPC
+        for(int i = 0; i < npc.length; i++){
+            if(npc[i] != null) {
+                npc[i].draw(g2);
+            }
+
         }
 
         // PLAYER
