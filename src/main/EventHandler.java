808d0d75d52811eb9b20ffee90cbf8e9ce1c4245
SergJac <sergiogjacquet@gmail.com>
2025-07-08 13:38:08 -0400

    Character Status

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -94,6 +94,7 @@ public class EventHandler {
     public void damagePit(int col, int row, int gameState) {
 
         gp.gameState = gameState;
+        gp.playSE(6);
         gp.ui.currentDialogue = "You fell into a pit!";
         gp.player.life -= 1;
 //        eventRect[col][row].eventDone = true;
@@ -104,6 +105,8 @@ public class EventHandler {
 
         if(gp.keyH.enterPressed == true) {
             gp.gameState = gameState;
+            gp.player.attackCanceled = true;
+            gp.playSE(2);
             gp.ui.currentDialogue = "You drank the water.\nYour health has been recovered!";
             gp.player.life = gp.player.maxLife;
         }
